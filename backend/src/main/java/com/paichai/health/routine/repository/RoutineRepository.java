d644f3f560fc1eb5d1ce1733f8ff93b3c51bdf32
jungchanyeop <jetwo6151@gmail.com>
2025-07-08 09:40:39 +0900

    routine 패키지 추가

---
+++ backend/src/main/java/com/paichai/health/routine/repository/RoutineRepository.java
@@ -0,0 +1,5 @@
+package com.paichai.health.routine.repository;
+
+public class RoutineRepository {
+
+}
---
+++ backend/src/main/java/com/paichai/health/routine/service/RoutineService.java
@@ -0,0 +1,5 @@
+package com.paichai.health.routine.service;
+
+public class RoutineService {
+
+}
